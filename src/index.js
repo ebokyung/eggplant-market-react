0d0c867edc5b12bf351718241df16ed4f8d20acc
jypark38 <jury124@naver.com>
2023-08-21 00:18:25 +0900

    chore : asset 추가 및 reset 추가 (#1)

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 // import ReactDOM from 'react-dom'; //구버전
 import { createRoot } from 'react-dom/client';
+import './styles/scss/reset.scss';
 import App from './App';
 
 const container = document.getElementById('root');
