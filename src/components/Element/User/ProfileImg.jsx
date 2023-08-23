96faee9448b198e71900e541773cf39d29c7efc1
jypark38 <jury124@naver.com>
2023-08-24 00:23:42 +0900

    comment : 주석 추가 및 제거 (#1)

--- src/components/Element/User/ProfileImg.jsx
+++ src/components/Element/User/ProfileImg.jsx
@@ -1,9 +1,9 @@
-/* eslint-disable import/no-unresolved */
 import React from 'react';
 import basicProfile from '../../../assets/basic-profile.png';
 import contrastProfile from '../../../assets/basic-profile-hc.png';
 import './style/ProfileImg.scss';
 
+// 이미지 처리 함수 추가 필요
 function ProfileImg({ profileImg, size, constrast = false }) {
   let image;
 
