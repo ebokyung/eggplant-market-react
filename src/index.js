e2457339bafc73e72a3bdec7f0823d45c6ee1765
jypark38 <jury124@naver.com>
2023-08-23 23:17:47 +0900

    chore : global 및 reset 설정 변경

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 // import ReactDOM from 'react-dom'; //구버전
 import { createRoot } from 'react-dom/client';
 import './styles/scss/reset.scss';
+import './styles/scss/global.scss';
 import App from './App';
 
 const container = document.getElementById('root');
