20d664a1a56058d5094381cbe9d916ab5702b1f7
ebokyung <dlqhrud303@naver.com>
2023-08-23 21:43:14 +0900

    design : 헤더에 버튼 컴포넌트 적용 (#4)

--- src/components/Element/Header/Header.jsx
+++ src/components/Element/Header/Header.jsx
@@ -1,35 +1,31 @@
 import React from 'react';
 import './Header.scss';
 import searchIcon from '../../../assets/icon/icon-search.svg';
-import optionIcon from '../../../assets/icon/icon-more-vertical.svg';
+import { Button, ButtonOption, ButtonBack } from '../Buttons/index';
 
-function Header(props) {
-  // props : home || search || follow || upload , text
+function Header({ page, text, btnText }) {
+  // props : page (main || search || upload || follow) , text , btnText
   return (
-    <header className="l_header header-main">
-      {props?.text && <h1 className={props?.home ? 'title-main' : 'title'}>{props?.text}</h1>}
-      {props?.home && (
+    <header className={`l_header header-${page || 'basic'}`}>
+      {text && <h1 className={page === 'main' ? 'title-main' : 'title'}>{text}</h1>}
+      {page === 'main' && (
         <button type="button">
           <img src={searchIcon} alt="검색하기" />
         </button>
       )}
-      {props?.search && (
+      {page === 'search' && (
         <label htmlFor="inp-search" className="l_inp-search">
           <span className="a11y-hidden">이름 혹은 계정으로 사용자 검색하기</span>
           <input type="text" id="inp-search" placeholder="이름 혹은 계정으로 사용자 검색하기" />
         </label>
       )}
-      {!(props?.home || props?.follow || props?.upload || props?.search) && (
-        <button type="button">
-          <img src={optionIcon} alt="설정하기" />
-        </button>
+      {page === 'upload' && (
+        <Button type="submit" className="size-ms" disabled>
+          {btnText || '업로드'}
+        </Button>
       )}
-      {props?.upload && <button type="button">업로드</button>}
-      {!props?.home && (
-        <button type="button" className="backbtn">
-          뒤로
-        </button>
-      )}
+      {!page && <ButtonOption />}
+      {page !== 'main' && <ButtonBack />}
     </header>
   );
 }
