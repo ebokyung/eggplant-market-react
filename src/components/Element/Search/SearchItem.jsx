057617dc2ef4f44f487efa58386362523e6e5fd4
jypark38 <jury124@naver.com>
2023-08-24 16:49:38 +0900

    design : 검색아이템 마크업 및 하이라이트 (#1)

--- src/components/Element/Search/SearchItem.jsx
+++ src/components/Element/Search/SearchItem.jsx
@@ -1,14 +1,29 @@
 import React from 'react';
 import { User } from '../User';
+import './SearchItem.scss';
 
-function SearchItem(user, keyword) {
-  const { _id, username, accountname } = user;
+function highlightKeyword(text, keyword) {
+  if (text.indexOf(keyword) !== -1) {
+    const parts = text.split(keyword);
+    return (
+      <>
+        {parts[0]}
+        <strong>{keyword}</strong>
+        {parts[1]}
+      </>
+    );
+  }
+  return text;
+}
 
+function SearchItem({ user, keyword }) {
+  const { _id, username, accountname, image } = user;
   // highlight 처리하기
+  const content = highlightKeyword(username, keyword);
 
   return (
     <li key={_id}>
-      <User category="search" userName={username} accountname={accountname} detail={accountname} />
+      <User profileImg={image} category="search" userName={content} accountName={accountname} detail={accountname} />
     </li>
   );
 }
