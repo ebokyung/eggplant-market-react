92c1e42f4da2948c7224948a1c72f23d8e126d85
jypark38 <jury124@naver.com>
2023-08-24 16:05:30 +0900

    design : children 제거 (#1)

--- src/components/Element/User/User.jsx
+++ src/components/Element/User/User.jsx
@@ -4,29 +4,15 @@ import ProfileImg from './ProfileImg';
 import UserInfo from './UserInfo';
 import './style/User.scss';
 
-function returnSize(category) {
-  switch (category) {
-    case 'post':
-    case 'chat':
-      return 'user';
-    case 'search':
-    case 'follow':
-      return 'item';
-    case 'comment':
-      return 'comment';
-    default:
-      return 'user';
-  }
-}
-
 // 링크 분기처리 필요
-export default function User({ category, accountName, userName, detail, children, profileImg }) {
+// 링크 분기 : profile 페이지의 경우 링크 작동 x
+// follow의 경우 UserInfo 내부의 p태크는 링크 작동 x
+export default function User({ category, accountName, userName, detail, profileImg }) {
   return (
     // eslint-disable-next-line jsx-a11y/anchor-is-valid
     <Link className="user-container" to={`./profile_info.html?accountName=${accountName}`}>
-      <ProfileImg profileImg={profileImg} size={returnSize(category)} />
+      <ProfileImg profileImg={profileImg} category={category} />
       <UserInfo category={category} userName={userName} detail={detail || accountName} />
-      {children}
     </Link>
   );
 }
