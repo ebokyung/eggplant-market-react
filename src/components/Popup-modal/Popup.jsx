61d098bd680ba5e48d9f388273e24e77f14d77b9
jypark38 <jury124@naver.com>
2023-08-26 23:00:56 +0900

    refactor : 의미없는 stoppropagation 제거 (#12)

--- src/components/Popup-modal/Popup.jsx
+++ src/components/Popup-modal/Popup.jsx
@@ -45,9 +45,8 @@ function Popup({ onClose, target, action }) {
   return (
     // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions
     <div
-      onClick={e => {
+      onClick={() => {
         onClose();
-        e.stopPropagation();
       }}
       className="modal-background"
     >
