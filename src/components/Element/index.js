b14df6f146dec160e7661683e67f70cbc569c14a
jypark38 <jury124@naver.com>
2023-08-27 15:13:43 +0900

    chore : index.js 추가 (#12)

--- src/components/Element/index.js
+++ src/components/Element/index.js
@@ -1,4 +1,9 @@
-// index
+import Footer from './Navbar/Navbar';
+import Header from './Header/Header';
+import Post from './Post/Post';
+import FollowItem from './Follow/FollowItem';
 
+// index
 export * from './Buttons';
 export * from './User';
+export { Footer, Header, Post };
