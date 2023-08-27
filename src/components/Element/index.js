af6eebaed30e69ec4abc07913b2552de4e18b7ff
jypark38 <jury124@naver.com>
2023-08-27 15:15:56 +0900

    remove : Element/index.js 제거

--- src/components/Element/index.js
+++
@@ -1,9 +0,0 @@
-import Footer from './Navbar/Navbar';
-import Header from './Header/Header';
-import Post from './Post/Post';
-import FollowItem from './Follow/FollowItem';
-
-// index
-export * from './Buttons';
-export * from './User';
-export { Footer, Header, Post };
