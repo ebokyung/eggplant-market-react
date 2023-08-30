23375d91ec6c4fb28ac5ebf13a82212d13683ec8
jypark38 <jury124@naver.com>
2023-08-30 22:33:01 +0900

    chore : 더미데이터 추가

---
+++ src/libs/dummy/storage.js
@@ -0,0 +1,2 @@
+export const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0OTgwZDMzYjJjYjIwNTY2MzlkNGM2YiIsImV4cCI6MTY5Nzk1NDYwMiwiaWF0IjoxNjkyNzcwNjAyfQ.O6GFZBT7jq4EkM58YuOkGBGkk24UeJWySMANtdGSE9E';
+export const accountname = 'jyp0308';
