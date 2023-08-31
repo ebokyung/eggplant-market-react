236507fb981b28594e7c8511677a44c9982f6d05
jypark38 <jury124@naver.com>
2023-08-31 18:35:59 +0900

    add : Home 라우터 추가

---
+++ src/pages/Home/routes/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from '../../../components/Element/Header/Header';
+import Footer from '../../../components/Element/Navbar/Navbar';
+import { HomeWithPost, HomeWithoutPost } from '../components';
+
+import { posts } from '../../../libs/dummy';
+import '../style/Home.scss';
+
+export function Home() {
+  const data = posts;
+  return (
+    <>
+      <Header page="main" text="가지마켓 피드" />
+      {data ? <HomeWithPost data={data} /> : <HomeWithoutPost />}
+      <Footer />
+    </>
+  );
+}
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
+import { Home } from '../pages/Home';
 
 export default function AppRoutes() {
   return (
     <Routes>
-      <Route path="/" element />
+      <Route path="/" element={<Home />} />
     </Routes>
   );
 }
