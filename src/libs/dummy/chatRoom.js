d2e41c891188bcf63b0dd1386036b6b65fb753ad
ebokyung <dlqhrud303@naver.com>
2023-09-01 19:08:29 +0900

    chore : 채팅방 dummy data 분리 (#19)

---
+++ src/libs/dummy/chatRoom.js
@@ -0,0 +1,91 @@
+import dummyImg from '../../assets/og(800x400).png';
+
+export default {
+  userName: '애월읍 가지밭',
+  contents: [
+    {
+      who: true,
+      body: '',
+      src: dummyImg,
+      time: '12:57',
+    },
+    {
+      who: true,
+      body: '네 말씀하세요',
+      time: '12:56',
+    },
+    {
+      who: false,
+      body: '안녕하세요. 감귤 사고싶어요요요요요',
+      time: '12:41',
+    },
+    {
+      who: false,
+      body: '옷을 인생을 그러므로 없으면 것은 이상은 것은 우리의 위하여, 뿐이다. 이상의 청춘의 뼈 따뜻한 그들의 그와 약동하다. 대고, 못할 넣는 풍부하게 뛰노는 인생의 힘있다.',
+      time: '12:39',
+    },
+    {
+      who: true,
+      body: '',
+      src: dummyImg,
+      time: '12:57',
+    },
+    {
+      who: true,
+      body: '네 말씀하세요',
+      time: '12:56',
+    },
+    {
+      who: false,
+      body: '안녕하세요. 감귤 사고싶어요요요요요',
+      time: '12:41',
+    },
+    {
+      who: false,
+      body: '옷을 인생을 그러므로 없으면 것은 이상은 것은 우리의 위하여, 뿐이다. 이상의 청춘의 뼈 따뜻한 그들의 그와 약동하다. 대고, 못할 넣는 풍부하게 뛰노는 인생의 힘있다.',
+      time: '12:39',
+    },
+    {
+      who: true,
+      body: '',
+      src: dummyImg,
+      time: '12:57',
+    },
+    {
+      who: true,
+      body: '네 말씀하세요',
+      time: '12:56',
+    },
+    {
+      who: false,
+      body: '안녕하세요. 감귤 사고싶어요요요요요',
+      time: '12:41',
+    },
+    {
+      who: false,
+      body: '옷을 인생을 그러므로 없으면 것은 이상은 것은 우리의 위하여, 뿐이다. 이상의 청춘의 뼈 따뜻한 그들의 그와 약동하다. 대고, 못할 넣는 풍부하게 뛰노는 인생의 힘있다.',
+      time: '12:39',
+    },
+    {
+      who: true,
+      body: '',
+      src: dummyImg,
+      time: '12:57',
+    },
+    {
+      who: true,
+      body: '네 말씀하세요',
+      time: '12:56',
+    },
+    {
+      who: false,
+      body: '안녕하세요. 감귤 사고싶어요요요요요',
+      time: '12:41',
+    },
+    {
+      who: false,
+      body: '옷을 인생을 그러므로 없으면 것은 이상은 것은 우리의 위하여, 뿐이다. 이상의 청춘의 뼈 따뜻한 그들의 그와 약동하다. 대고, 못할 넣는 풍부하게 뛰노는 인생의 힘있다.',
+      time: '12:39',
+    },
+  ],
+};
