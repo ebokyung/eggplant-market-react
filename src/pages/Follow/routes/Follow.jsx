423c451bb2f824e0fb9c368d77e9d213d6c05f89
jypark38 <jury124@naver.com>
2023-09-05 00:52:41 +0900

    design : follow-item 관련 스타일 (#32)

---
+++ src/pages/Follow/routes/Follow.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { useLocation } from 'react-router-dom';
+import Header from '../../../components/Element/Header/Header';
+import Navbar from '../../../components/Element/Navbar/Navbar';
+import { FollowList } from '../components/FollowList';
+import '../style/Follow.scss';
+
+export function Follow() {
+  const location = useLocation();
+  const isFollower = location.pathname.includes('follower');
+
+  return (
+    <>
+      <Header page="follow" text={isFollower ? 'Follower' : 'Following'} />
+      <FollowList isFollower={isFollower} />
+      <Navbar />
+    </>
+  );
+}
