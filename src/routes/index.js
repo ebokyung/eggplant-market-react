8e551e65ed7fc4ce8e528ceca9975243c2f25a3a
jypark38 <jury124@naver.com>
2023-09-04 18:08:25 +0900

    add : route 추가 (#21)

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { Home } from '../pages/Home';
+import { Search } from '../pages/Search';
 
 export default function AppRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/search" element={<Search />} />
     </Routes>
   );
 }
