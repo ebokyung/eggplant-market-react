538eae0430c3c65454ff642af415d9929f501d94
jypark38 <jury124@naver.com>
2023-09-05 00:53:54 +0900

    refactor : li요소 외부로 빼고 props 정리

---
+++ src/pages/Follow/components/FollowItem.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { User } from '../../../components/Element/User';
+import FollowButton from './FollowButton';
+
+function FollowItem({ user }) {
+  const { accountname, image, intro, isfollow, username } = user;
+
+  const UserProp = {
+    category: 'follow',
+    accountName: accountname,
+    userName: username,
+    detail: intro,
+    profileImg: image,
+  };
+
+  return (
+    <>
+      <User {...UserProp} />
+      <FollowButton isFollow={isfollow} />
+    </>
+  );
+}
+
+export default FollowItem;
