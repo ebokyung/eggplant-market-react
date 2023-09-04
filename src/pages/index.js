7b1202116eab6b5a9811797fb169daa8b7c03337
ebokyung <dlqhrud303@naver.com>
2023-09-04 14:54:26 +0900

    design : 로그인 페이지 마크업 및 스타일 (#26)

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,2 +1,3 @@
 export { Error } from './Error/Error';
 export { Splash } from './Splash/Splash';
+export { Login } from './Login/Login';
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import { Error, Splash } from '../pages';
+import { Error, Splash, Login } from '../pages';
 
 export default function AppRoutes() {
   return (
     <Routes>
       <Route path="/error" element={<Error />} />
       <Route path="/splash" element={<Splash />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
   );
 }
