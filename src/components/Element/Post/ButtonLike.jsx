54646cfb1d1d6491fff96060e70875c99770d2be
jypark38 <jury124@naver.com>
2023-09-05 01:05:43 +0900

    add : 좋아요 버튼에 id 프롭 추가

--- src/components/Element/Post/ButtonLike.jsx
+++ src/components/Element/Post/ButtonLike.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
 
-function ButtonLike({ hearted, heartCount }) {
+function ButtonLike({ id, hearted, heartCount }) {
   const [isHeart, setIsHeart] = useState(hearted);
   const [heartCnt, setHeartCnt] = useState(heartCount);
+  // 좋아요 기능 처리용 id
+  console.log(id);
 
   function handleLike() {
     setIsHeart(prev => !prev);
