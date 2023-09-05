cd0ca664ee664da115cfd07622e9f4020a32fd8c
ebokyung <dlqhrud303@naver.com>
2023-09-05 17:08:04 +0900

    add : view tab에 따른 게시글 렌더링 (#25)

--- src/pages/Profile/components/PostSection.jsx
+++ src/pages/Profile/components/PostSection.jsx
@@ -1,15 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/PostSection.scss';
 import { PostViewTab, ListView, AlbumView } from './PostSection/index';
 
 export function PostSection() {
+  const [listViewOn, isListViewOn] = useState(true);
+
   return (
     <section id="post" className="post-container">
-      <PostViewTab />
-      <section className="post-sec">
-        <ListView />
-        <AlbumView />
-      </section>
+      <PostViewTab listViewOn={listViewOn} isListViewOn={isListViewOn} />
+      <section className="post-sec">{listViewOn ? <ListView /> : <AlbumView />}</section>
     </section>
   );
 }
--- src/pages/Profile/components/PostSection/PostViewTab.jsx
+++ src/pages/Profile/components/PostSection/PostViewTab.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
-export function PostViewTab() {
+export function PostViewTab({ listViewOn, isListViewOn }) {
   return (
     <section className="post-tab">
       <h2 className="a11y-hidden">게시글 보기 유형 고르기</h2>
       <div className="tab-btn-wrap">
-        <button type="button" className="btn-post-list on">
+        <button type="button" className={`btn-post-list ${listViewOn ? 'on' : ''}`} onClick={() => isListViewOn(true)}>
           <span className="a11y-hidden">목록형</span>
         </button>
-        <button type="button" className="btn-post-album">
+        <button type="button" className={`btn-post-album ${listViewOn ? '' : 'on'}`} onClick={() => isListViewOn(false)}>
           <span className="a11y-hidden">앨범형</span>
         </button>
       </div>
