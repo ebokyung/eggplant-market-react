e08259994a8eac5b4158f677a2d7def09e67e679
ebokyung <dlqhrud303@naver.com>
2023-09-05 21:38:06 +0900

    chore : dummy data 수정 및 적용 (#25)

--- src/pages/Profile/components/ProductSection/ProductItem.jsx
+++ src/pages/Profile/components/ProductSection/ProductItem.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
 export function ProductItem({ item }) {
-  const { id, itemImg, itemName, price } = item;
+  const { id, itemImage, itemName, price } = item;
   return (
     <button className="product" data-product-id={id} type="button">
       <strong className="product-name">{itemName}</strong>
-      <img className="product-img" src={itemImg} alt="" />
+      <img className="product-img" src={itemImage} alt="" />
       <strong className="product-price">
         <span className="price">{Number(price).toLocaleString()}</span>원
       </strong>
--- src/pages/Profile/routes/UserProfile.jsx
+++ src/pages/Profile/routes/UserProfile.jsx
@@ -3,15 +3,16 @@ import '../styles/UserProfile.scss';
 import Header from '../../../components/Element/Header/Header';
 import Navbar from '../../../components/Element/Navbar/Navbar';
 import { ProfileSection, ProductSection, PostSection } from '../components';
+import { userProfile, userProduct, userPost } from '../../../libs/dummy';
 
 export function UserProfile() {
   return (
     <>
       <Header />
       <main className="main-with-nav">
-        <ProfileSection />
-        <ProductSection />
-        <PostSection />
+        <ProfileSection data={userProfile.profile} />
+        {!!userProduct.data && <ProductSection data={userProduct.product} />}
+        <PostSection data={userPost} />
       </main>
       <Navbar />
     </>
