32f8e37c250fd34fa1e34fea1d43bc858d6731bb
jypark38 <jury124@naver.com>
2023-09-07 17:44:42 +0900

    add : router추가 (#91)

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,21 +1,23 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import { Error, Splash, Login } from '../pages';
+// import { Error, Splash, Login } from '../pages';
 import { Home } from '../pages/Home';
 import { Search } from '../pages/Search';
-import { ChatList } from '../pages/ChatList';
-import { ChatRoom } from '../pages/ChatRoom';
+// import { ChatList } from '../pages/ChatList';
+// import { ChatRoom } from '../pages/ChatRoom';
+import { PostDetail } from '../pages/Post';
 
 export default function AppRoutes() {
   return (
     <Routes>
-      <Route path="/error" element={<Error />} />
-      <Route path="/splash" element={<Splash />} />
-      <Route path="/login" element={<Login />} />
+      {/* <Route path="/error" element={<Error />} /> */}
+      {/* <Route path="/splash" element={<Splash />} /> */}
+      {/* <Route path="/login" element={<Login />} /> */}
       <Route path="/home" element={<Home />} />
       <Route path="/search" element={<Search />} />
-      <Route path="/chat" element={<ChatList />} />
-      <Route path="/chat-room" element={<ChatRoom />} />
+      {/* <Route path="/chat" element={<ChatList />} /> */}
+      {/* <Route path="/chat-room" element={<ChatRoom />} /> */}
+      <Route path="/post" element={<PostDetail />} />
     </Routes>
   );
 }
