913ce614445477d2e81f7147c91330c5b0e75889
ebokyung <dlqhrud303@naver.com>
2023-09-19 14:44:51 +0900

    chore : 피드백 반영 - post section 게시물 없을때 렌더링 안함 (#25)

--- src/pages/Profile/routes/UserProfile.jsx
+++ src/pages/Profile/routes/UserProfile.jsx
@@ -12,7 +12,7 @@ export function UserProfile() {
       <main className="main-with-nav main-user-profile">
         <ProfileSection data={userProfile.profile} />
         {!!userProduct.data && <ProductSection data={userProduct.product} />}
-        <PostSection data={userPost.post} />
+        {!!userPost.post && <PostSection data={userPost.post} />}
       </main>
       <Navbar />
     </>
