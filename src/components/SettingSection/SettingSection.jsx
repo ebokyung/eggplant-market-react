6ec939576bc67a3478e7bd9d21b34a647f08b998
jypark38 <jury124@naver.com>
2023-09-19 14:20:57 +0900

    add : signup 페이지 추가

---
+++ src/components/SettingSection/SettingSection.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { ImgSection } from './atoms/ImgSection';
+
+export function SettingSection() {
+  return (
+    <main className="signup-setting-main">
+      <form action="">
+        <ImgSection />
+      </form>
+    </main>
+  );
+}
