1535658fb38292099854ade317d2dc91c905fefd
jypark38 <jury124@naver.com>
2023-09-21 23:38:47 +0900

    add : 유저 정보 입력 섹션 (#46)

---
+++ src/components/Information/Information.jsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import { InputImg } from '../Element/Input/InputImg';
+import { InputUsername, InputAcctname, InputIntro } from '../InputComponents';
+
+export function Information({ initialData, setIsCompleteDisabled }) {
+  const isSignUp = useLocation().pathname.includes('sign-up');
+  const profile = initialData ?? null;
+
+  const [usernameError, setUsernameError] = useState({
+    isError: isSignUp,
+    errorText: '',
+  });
+  const [accountnameError, setAccountnameError] = useState({
+    isError: isSignUp,
+    errorText: '',
+  });
+
+  useEffect(() => {
+    if (usernameError.isError || accountnameError.isError) {
+      setIsCompleteDisabled(true);
+    } else {
+      setIsCompleteDisabled(false);
+    }
+  }, [usernameError, accountnameError]);
+
+  return (
+    <>
+      <InputImg initialImg={profile ? profile.image : undefined} />
+      <InputUsername initialValue={profile ? profile.username : ''} error={usernameError} seterror={setUsernameError} />
+      <InputAcctname isSignUp={isSignUp} initialValue={profile ? profile.accountname : ''} error={accountnameError} seterror={setAccountnameError} />
+      <InputIntro initialValue={profile ? profile.intro : ''} />
+    </>
+  );
+}
