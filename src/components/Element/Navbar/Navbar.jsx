b0aecf5fb191c16eae835e08a027ec7aab3ce435
ebokyung <dlqhrud303@naver.com>
2023-10-05 17:23:56 +0900

    add : navbar 클릭시 active 스타일 적용 (#4)

--- src/components/Element/Navbar/Navbar.jsx
+++ src/components/Element/Navbar/Navbar.jsx
@@ -1,29 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Navbar.scss';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 function Navbar() {
+  const location = useLocation();
+  const [curPage, setCurPage] = useState(location.pathname);
+  console.log(curPage);
+
   return (
     <nav className="tab-menu">
       <p className="a11y-hidden">하단 탭 메뉴</p>
       <ul className="tab-item-list">
         <li className="tab-item-home">
-          <Link to="/home">
+          <Link to="/home" onClick={() => setCurPage('/home')} className={curPage === '/home' ? 'here' : undefined}>
             홈<span className="a11y-hidden">으로 이동</span>
           </Link>
         </li>
         <li className="tab-item-chat">
-          <Link to="/chat">
+          <Link to="/chat" onClick={() => setCurPage('/chat')} className={curPage === '/chat' ? 'here' : undefined}>
             채팅<span className="a11y-hidden">으로 이동</span>
           </Link>
         </li>
         <li className="tab-item-follow">
-          <Link to="/post-upload">
+          <Link to="/posting" onClick={() => setCurPage('/posting')} className={curPage === '/posting' ? 'here' : undefined}>
             게시물 작성<span className="a11y-hidden">으로 이동</span>
           </Link>
         </li>
         <li className="tab-item-more">
-          <Link to="/profile">
+          <Link to="/profile" onClick={() => setCurPage('/profile')} className={curPage === '/profile' ? 'here' : undefined}>
             프로필<span className="a11y-hidden">로 이동</span>
           </Link>
         </li>
