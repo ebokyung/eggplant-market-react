4b78cadfa4f1471dd6c236b7305ba12d0142923b
ebokyung <dlqhrud303@naver.com>
2023-10-10 21:56:58 +0900

    add : 상품 이미지 POST 및 상품 업로드 POST 요청 (#28)

--- src/pages/Product/api/index.js
+++ src/pages/Product/api/index.js
@@ -1,9 +1,32 @@
-import { defaultAxios } from '../../../libs/axios';
+import { defaultAxios, postImage } from '../../../libs/axios';
+import { API_URL } from '../../../configs/config';
 
 export const getProductAPI = async productId => {
   try {
     const res = await defaultAxios.get(`/product/detail/${productId}`);
     return res.data;
+  } catch (e) {
+    console.log(e);
+    return e;
+  }
+};
+
+export const postProductAPI = async data => {
+  try {
+    const res = await defaultAxios.post(`/product`, data);
+    return res;
+  } catch (e) {
+    console.log(e);
+    return e;
+  }
+};
+
+export const postProductImgAPI = async img => {
+  try {
+    const formData = new FormData();
+    formData.append('image', img);
+    const res = await postImage.post(`/image/uploadfile`, formData);
+    return `${API_URL}/${res.filename}`;
   } catch (e) {
     console.log(e);
     return e;
