e684cfb72e206075a7bb4c22c25f20de804a9e87
jypark38 <jury124@naver.com>
2023-10-12 17:20:27 +0900

    fix

--- src/components/Element/User/UserInfo.jsx
+++ src/components/Element/User/UserInfo.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 import React from 'react';
 import './style/UserInfo.scss';
