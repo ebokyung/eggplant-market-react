ef40f7ca0e6403daf8a427ac942b0977d71c08c6
jypark38 <jury124@naver.com>
2023-10-12 17:17:59 +0900

    add : home get 요청 (#55)

--- src/pages/Home/routes/Home.jsx
+++ src/pages/Home/routes/Home.jsx
@@ -1,16 +1,28 @@
-import React from 'react';
+/* eslint-disable no-nested-ternary */
+import React, { useEffect, useState } from 'react';
 import Header from '../../../components/Element/Header/Header';
 import Footer from '../../../components/Element/Navbar/Navbar';
-import { HomeWithPost, HomeWithoutPost } from '../components';
-
-import { posts } from '../../../libs/dummy';
+import { defaultAxios } from '../../../libs/api/axios';
+import '../style/Home.scss';
+import { HomeWithPost } from '../components/HomeWithPost';
+import { HomeWithoutPost } from '../components/HomeWithoutPost';
 
 export function Home() {
-  const data = posts;
+  const [posts, setPosts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    (async () => {
+      const { data } = await defaultAxios.get('/post/feed/');
+      setPosts(data.posts);
+      setIsLoading(false);
+    })();
+  }, []);
+
   return (
     <>
       <Header page="main" text="가지마켓 피드" />
-      {data.posts.length ? <HomeWithPost data={data} /> : <HomeWithoutPost />}
+      {isLoading ? <main>스켈레톤이 들어갈 자리</main> : posts.length ? <HomeWithPost posts={posts} /> : <HomeWithoutPost />}
       <Footer />
     </>
   );
