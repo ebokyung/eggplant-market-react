35eaf0c760f110278396b116e4f49dc6970b476f
jypark38 <jury124@naver.com>
2023-10-12 17:15:56 +0900

    add : image 처리 함수 추가 (#55)

--- src/pages/Profile/components/ProductSectionElement/ProductItem.jsx
+++ src/pages/Profile/components/ProductSectionElement/ProductItem.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import { checkImageUrl } from '../../../../utils/imageUrlProcess';
 
 export function ProductItem({ item }) {
   const { id, itemImage, itemName, price } = item;
   return (
     <button className="product" data-product-id={id} type="button">
       <strong className="product-name">{itemName}</strong>
-      <img className="product-img" src={itemImage} alt="" />
+      <img className="product-img" src={checkImageUrl(itemImage, 'post')} alt="" />
       <strong className="product-price">
         <span className="price">{Number(price).toLocaleString()}</span>원
       </strong>
