d06b0e75a166994991d3118b787ba6245abe83d4
ebokyung <dlqhrud303@naver.com>
2023-10-15 17:52:31 +0900

    add : 프로필 페이지 GET요청 (#25)

---
+++ src/pages/Profile/api/index.js
@@ -0,0 +1,51 @@
+import { defaultAxios } from '../../../libs/axios';
+
+export const getProfileAPI = async accountname => {
+  try {
+    const res = await defaultAxios.get(`/profile/${accountname}`);
+    return res.data;
+  } catch (e) {
+    console.log(e);
+    return e;
+  }
+};
+
+export const getProductAPI = async accountname => {
+  try {
+    const res = await defaultAxios.get(`/product/${accountname}`);
+    return res.data;
+  } catch (e) {
+    console.log(e);
+    return e;
+  }
+};
+
+export const getPostAPI = async accountname => {
+  try {
+    const res = await defaultAxios.get(`/post/${accountname}/userpost`);
+    return res.data;
+  } catch (e) {
+    console.log(e);
+    return e;
+  }
+};
+
+export const postFollowAPI = async someone => {
+  try {
+    const res = await defaultAxios.post(`/profile/${someone}/follow`);
+    return res.data;
+  } catch (e) {
+    console.log(e);
+    return e;
+  }
+};
+
+export const deleteFollowAPI = async someone => {
+  try {
+    const res = await defaultAxios.delete(`/profile/${someone}/unfollow`);
+    return res.data;
+  } catch (e) {
+    console.log(e);
+    return e;
+  }
+};
