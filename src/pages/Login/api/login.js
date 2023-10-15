95c15e1a97afd603c8264f328a70b9de8cf5d4ff
jypark38 <jury124@naver.com>
2023-10-15 17:09:57 +0900

    add : login api 구현 + 연결 (#57)

---
+++ src/pages/Login/api/login.js
@@ -0,0 +1,23 @@
+import { defaultAxios } from '../../../libs/axios';
+import { storage } from '../../../utils/storage';
+
+// 둘다 공백 : "이메일 또는 비밀번호를 입력해주세요."
+// 이메일만 공백 : "이메일을 입력해주세요."
+// 비밀번호 공백 : "패스워드를 입력해주세요."
+// 로그인 실패 "이메일 또는 비밀번호가 일치하지 않습니다."
+// "status": 422
+
+export const signInApi = async loginInfo => {
+  try {
+    const res = await defaultAxios.post('/user/login', { user: loginInfo });
+
+    if (res.data.status === 422) {
+      return res.data;
+    }
+    storage.setToken(res.data.user.token);
+    storage.setAccountName(res.data.user.accountname);
+    return { status: 200 };
+  } catch (e) {
+    return e;
+  }
+};
