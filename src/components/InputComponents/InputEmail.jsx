f9275fcd0956811ba9144f4f33ee1b153e908711
ebokyung <dlqhrud303@naver.com>
2023-10-16 16:15:01 +0900

    add : 이메일 중복 여부 검사 (#46)

--- src/components/InputComponents/InputEmail.jsx
+++ src/components/InputComponents/InputEmail.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import { defaultAxios } from '../../libs/api/axios';
 import { Input } from '../Element/Input';
 
 export function InputEmail({ error, seterror }) {
+  async function validateEmail(email) {
+    try {
+      const res = await defaultAxios.post(`/user/emailvalid`, {
+        user: {
+          email,
+        },
+      });
+      return res.data.message;
+    } catch (e) {
+      return e;
+    }
+  }
+
   const emailProps = {
     name: 'email',
     inputId: 'email',
@@ -14,16 +28,28 @@ export function InputEmail({ error, seterror }) {
     onBlur: e => {
       const valid = e.target.validity;
       if (!valid.valueMissing) {
-        if (valid.typeMismatch) {
+        const emailRegEx = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[a-zA-Z]+([-_.]?[a-zA-Z])*.[a-zA-Z]{2,3}$/i;
+        if (!emailRegEx.test(e.target.value)) {
           seterror({
             isError: true,
-            errorText: '유효하지 않은 형식',
+            errorText: '유효하지 않은 형식입니다.',
           });
         } else {
-          seterror({
-            isError: false,
-            errorText: '사용 가능한 이메일 입니다.',
-          });
+          // 유효성 검사
+          (async () => {
+            const resMsg = await validateEmail(e.target.value);
+            if (resMsg === '사용 가능한 이메일 입니다.') {
+              seterror({
+                isError: false,
+                errorText: resMsg,
+              });
+            } else if (resMsg === '이미 가입된 이메일 주소 입니다.') {
+              seterror({
+                isError: true,
+                errorText: resMsg,
+              });
+            }
+          })();
         }
       } else {
         seterror({
