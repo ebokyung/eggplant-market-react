4178a3c0ee75cd8b0719295e78d3d13c191f9620
jypark38 <jury124@naver.com>
2023-10-17 20:00:31 +0900

    Add : util 적용 (#48)

--- src/pages/Posting/components/ImageItem.jsx
+++ src/pages/Posting/components/ImageItem.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import '../style/ImageItem.scss';
 import closeIcon from '../../../assets/icon/x.svg';
+import { checkImageUrl } from '../../../utils/imageUrlProcess';
 
 export function ImageItem({ img, onRemove }) {
   return (
     <div className="posting-img-cover">
-      <img src={img instanceof File ? URL.createObjectURL(img) : `https://api.mandarin.weniv.co.kr/${img}`} alt="" />
+      <img src={img instanceof File ? URL.createObjectURL(img) : checkImageUrl(img, 'post')} alt="" />
       <button type="button" className="btn-remove" onClick={() => onRemove(img)}>
         <img src={closeIcon} alt="" />
       </button>
