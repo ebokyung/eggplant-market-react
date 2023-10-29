5848c86d47058bb96473c057c86a203210c0cc0d
ebokyung <dlqhrud303@naver.com>
2023-10-29 20:41:53 +0900

    chore : 태그명, prop명 변경 및 옵셔널체이닝 추가

--- src/components/Element/Modal/Alert.jsx
+++ src/components/Element/Modal/Alert.jsx
@@ -2,34 +2,34 @@ import React from 'react';
 import './Alert.scss';
 import { useSetRecoilState, useRecoilValue } from 'recoil';
 import { createPortal } from 'react-dom';
-import { openAlert, doAlert } from '../../../recoil/modal/atoms';
+import { isAlertOpen, doAlert } from '../../../recoil/modal/atoms';
 
-function Alert({ handleModal }) {
-  const isAlert = useSetRecoilState(openAlert);
+function Alert({ closeModal }) {
+  const isAlert = useSetRecoilState(isAlertOpen);
   const doFunc = useRecoilValue(doAlert);
 
   const renderAlert = (
-    <div className="modal-background">
-      <h2 className="a11y-hidden">팝업</h2>
+    <dialog className="modal-background">
+      <h2 className="a11y-hidden">확인창</h2>
       <div className="modal-content">
-        <p className="modal-description">{doFunc.text}하시겠습니까?</p>
+        <p className="modal-description">{doFunc?.text}하시겠습니까?</p>
         <div className="modal-actions">
           <button
             type="button"
             onClick={() => {
-              doFunc.func();
+              doFunc?.func();
               isAlert(false);
-              handleModal();
+              closeModal();
             }}
           >
-            {doFunc.text}
+            {doFunc?.text}
           </button>
           <button type="button" onClick={() => isAlert(false)}>
             취소
           </button>
         </div>
       </div>
-    </div>
+    </dialog>
   );
 
   return createPortal(renderAlert, document.getElementById('root'));
