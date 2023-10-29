6bb599cac422c6a27221d4377aa34ca48715e902
ebokyung <dlqhrud303@naver.com>
2023-10-29 15:32:42 +0900

    add : header에 버튼 스켈레톤 넘겨주기 & chore : 검색 페이지 마크업 제거 (#4)

--- src/components/Element/Header/Header.jsx
+++ src/components/Element/Header/Header.jsx
@@ -5,12 +5,13 @@ import searchIcon from '../../../assets/icon/icon-search.svg';
 import { Button, ButtonOption, ButtonBack } from '../Buttons';
 
 function Header({ page, text, btnText, children, className, btnDisabled = true, formName }) {
-  // props : page (main || search || upload || follow) , text , btnText
+  // props : page (main || upload || follow) , text , btnText
   return (
     <header className={`l_header header-${page || 'basic'} ${className}`}>
       {text && (
         <h1 className={page === 'main' ? 'title-main' : 'title'}>
           {text}
+          {/* expected children : hidden span tag */}
           {children}
         </h1>
       )}
@@ -19,17 +20,17 @@ function Header({ page, text, btnText, children, className, btnDisabled = true, 
           <img src={searchIcon} alt="검색하기" />
         </Link>
       )}
-      {page === 'search' && (
-        <label htmlFor="inp-search" className="l_inp-search">
-          <span className="a11y-hidden">이름 혹은 계정으로 사용자 검색하기</span>
-          <input type="text" id="inp-search" placeholder="이름 혹은 계정으로 사용자 검색하기" />
-        </label>
-      )}
-      {page === 'upload' && (
-        <Button type="submit" className="size-ms" disabled={btnDisabled} form={formName}>
-          {btnText || '업로드'}
-        </Button>
-      )}
+      {page === 'upload' &&
+        (children ? (
+          <div>
+            {/* expected children : button skeleton for loading state */}
+            {children}
+          </div>
+        ) : (
+          <Button type="submit" className="size-ms" disabled={btnDisabled} form={formName}>
+            {btnText || '업로드'}
+          </Button>
+        ))}
       {!page && <ButtonOption />}
       {page !== 'main' && <ButtonBack />}
     </header>
