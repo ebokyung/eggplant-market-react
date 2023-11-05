de5c1f605d0b3ff467d110e13afbfcdd3d6a5be1
jypark38 <jury124@naver.com>
2023-11-05 20:23:01 +0900

    add : 업로드 가능한 image 확장자 수정

--- src/pages/ChatRoom/routes/ChatRoom.jsx
+++ src/pages/ChatRoom/routes/ChatRoom.jsx
@@ -57,7 +57,7 @@ export function ChatRoom() {
               />
               <path d="M26.2502 20.7498L21.6668 16.1665L11.5835 26.2498" stroke="white" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
             </svg>
-            <input id="inputImg" type="file" accept="image/*" />
+            <input id="inputImg" type="file" accept=".jpg, .gif, .png, .jpeg, .bmp, .tif, .heic" />
           </label>
           <button type="submit" className="btn-send" disabled>
             <span className="a11y-hidden">메세지</span>전송
