239883e07af6b79a59a81bb6e58212b471e6ed56
jypark38 <jury124@naver.com>
2023-12-02 15:32:48 +0900

    refactor : 안쓰는 dummy import 제거

--- src/libs/dummy/index.js
+++ src/libs/dummy/index.js
@@ -1,13 +1,2 @@
-export * from './posts';
-export * from './product';
-export * from './search';
 export * from './chatRoom';
 export * from './chatListData';
-export * from './myData';
-// profile product post followers followings
-export * from './post';
-// myinfo singlePost commentList
-export * from './storage';
-// token accountname
-export * from './userData';
-// userProfile userProduct userPost
