81b01086c77a612e780997d6825cb113593723ad
ebokyung <dlqhrud303@naver.com>
2024-02-25 14:18:58 +0900

    feat: 사이드바에 더보기 버튼 추가

--- src/components/Element/Navbar/Navbar.jsx
+++ src/components/Element/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import './Navbar.scss';
 import { Link, useLocation } from 'react-router-dom';
 import { storage } from '../../../utils/storage';
+import { ButtonOption } from '../Buttons';
 
 function Navbar() {
   const location = useLocation();
@@ -11,7 +12,7 @@ function Navbar() {
 
   return (
     <nav className="tab-menu">
-      <p className="a11y-hidden">하단 탭 메뉴</p>
+      <p className="a11y-hidden">메뉴</p>
       <ul className="tab-item-list">
         <li className="tab-item-home">
           <Link id="footer" to="/home" onClick={() => setCurPage('/home')} className={curPage === '/home' ? 'here' : undefined}>
@@ -28,10 +29,13 @@ function Navbar() {
             게시물 작성<span className="a11y-hidden">으로 이동</span>
           </Link>
         </li>
-        <li className="tab-item-more">
+        <li className="tab-item-profile">
           <Link to={`/profile?accountName=${accountName}`} onClick={() => setCurPage('/profile')} className={curPage === '/profile' && isMyPage ? 'here' : undefined}>
             프로필<span className="a11y-hidden">로 이동</span>
           </Link>
+        </li>
+        <li className="tab-item-more">
+          <ButtonOption isViewText />
         </li>
       </ul>
     </nav>
