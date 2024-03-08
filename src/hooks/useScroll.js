d1c8b129896fc891d187df5427cff0a0a6894afe
ebokyung <dlqhrud303@naver.com>
2024-03-08 13:49:41 +0900

    refactor: fetchClosure, scroll 함수 -> useScroll hook

---
+++ src/hooks/useScroll.js
@@ -0,0 +1,47 @@
+import { throttle } from 'lodash';
+import { useCallback, useEffect, useState } from 'react';
+import { getDocumentHeight, getScrollTop } from '../utils/scroll';
+
+export const useScroll = ({ getData, setData, setIsLoading }) => {
+  const [page, setPage] = useState(0);
+  const [hasMoreData, setHasMoreData] = useState(true);
+
+  const handleScroll = throttle(async () => {
+    if (Math.ceil(getScrollTop()) >= getDocumentHeight() - window.innerHeight) {
+      setPage(prev => prev + 1);
+    }
+  }, 500);
+  const handleScrollEvent = useCallback(handleScroll);
+
+  const loadData = async () => {
+    const res = await getData(page);
+    if (Array.isArray(res.data)) {
+      if (res.data.length === 0) {
+        setHasMoreData(false);
+        return;
+      }
+      setData(prev => prev.concat(res.data));
+    } else {
+      if (Object.values(res.data)[0].length === 0) {
+        setHasMoreData(false);
+        return;
+      }
+      setData(prev => prev.concat(Object.values(res.data)[0]));
+    }
+  };
+
+  useEffect(() => {
+    if (hasMoreData) loadData();
+    else {
+      window.removeEventListener('scroll', handleScrollEvent);
+    }
+    setIsLoading(false);
+  }, [page]);
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScrollEvent);
+    return () => {
+      window.removeEventListener('scroll', handleScrollEvent);
+    };
+  }, []);
+};
