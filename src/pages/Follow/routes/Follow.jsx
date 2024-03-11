d04a25060e4bdc534e774f0d1b26ab8e35bfd2fb
ebokyung <dlqhrud303@naver.com>
2024-03-11 16:07:03 +0900

    refactor: 팔로워/팔로잉 목록에 withInfiniteScroll 적용하여 데이터 페칭관련 관심사 분리

--- src/pages/Follow/routes/Follow.jsx
+++ src/pages/Follow/routes/Follow.jsx
@@ -1,27 +1,28 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useLocation } from 'react-router-dom';
 import Header from '../../../components/Element/Header/Header';
 import Navbar from '../../../components/Element/Navbar/Navbar';
 import FollowItem from '../components/FollowItem';
 import '../style/Follow.scss';
-import SkeletonFollowItem from '../components/SkeletonFollowItem';
-import { useScroll } from '../../../hooks/useScroll';
+// import SkeletonFollowItem from '../components/SkeletonFollowItem';
 import { Meta } from '../../../libs/Meta';
-import { defaultAxios } from '../../../libs/api/axios';
+import withInfiniteScroll from '../../../hocs/withInfinityScroll';
+
+function FollowList({ data }) {
+  return data.map(item => (
+    <li key={item._id} className="follow-item">
+      <FollowItem user={item} />
+    </li>
+  ));
+}
 
 export default function Follow() {
-  const [data, setData] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
   const location = useLocation();
   const isFollower = location.pathname.includes('follower');
   const accountName = new URLSearchParams(location.search).get('accountName');
 
-  const VIEW_COUNT = 11;
-  const getData = async page => {
-    return defaultAxios.get(`/profile/${accountName}/${isFollower ? 'follower' : 'following'}?skip=${page * VIEW_COUNT}&limit=${VIEW_COUNT}`);
-  };
-
-  useScroll({ getData, setData, setIsLoading });
+  const VIEW_COUNT = 2;
+  const WithInfinityScrollFollowList = withInfiniteScroll(FollowList, `/profile/${accountName}/${isFollower ? 'follower' : 'following'}`, VIEW_COUNT);
 
   return (
     <>
@@ -29,15 +30,9 @@ export default function Follow() {
       <Header page="follow" text={isFollower ? 'Follower' : 'Following'} />
       <main className="main-with-nav">
         <ul className="follow-list">
-          {isLoading ? (
-            <SkeletonFollowItem />
-          ) : (
-            data?.map(followitem => (
-              <li key={followitem._id} className="follow-item">
-                <FollowItem user={followitem} />
-              </li>
-            ))
-          )}
+          {/* <Suspense fallback={<SkeletonFollowItem />}> */}
+          <WithInfinityScrollFollowList />
+          {/* </Suspense> */}
         </ul>
       </main>
       <Navbar />
