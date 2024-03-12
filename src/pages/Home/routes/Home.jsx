1ac27027eb133b7477ea3bb6d1aca9f870702439
ebokyung <dlqhrud303@naver.com>
2024-03-12 20:06:12 +0900

    refactor: 홈 피드에 변경된 useScroll hook 적용

--- src/pages/Home/routes/Home.jsx
+++ src/pages/Home/routes/Home.jsx
@@ -1,8 +1,10 @@
 /* eslint-disable no-nested-ternary */
-import React, { useState } from 'react';
+// ---------- Hook 사용시 ---------
+import React from 'react';
 import Header from '../../../components/Element/Header/Header';
 import Footer from '../../../components/Element/Navbar/Navbar';
 import '../style/Home.scss';
+import StylesSpinner from '../../../hocs/spinner.module.scss';
 import HomeWithPost from '../components/HomeWithPost';
 import HomeWithoutPost from '../components/HomeWithoutPost';
 import SkeletonHome from '../components/SkeletonHome';
@@ -12,22 +14,34 @@ import { SkipNav } from '../../../components/Element/SkipNav/SkipNav';
 import { defaultAxios } from '../../../libs/api/axios';
 
 export default function Home() {
-  const [posts, setPosts] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-
   const VIEW_COUNT = 6;
-  const getData = async page => {
+
+  const fetchPosts = async page => {
     return defaultAxios.get(`/post/feed?skip=${page * VIEW_COUNT}&limit=${VIEW_COUNT}`);
   };
 
-  useScroll({ getData, setData: setPosts, setIsLoading });
+  const { data: posts, fetchStatus } = useScroll(fetchPosts);
 
   return (
     <>
       <Meta title="피드" />
       <SkipNav page="home" />
       <Header page="main" text="가지마켓 피드" />
-      {isLoading ? <SkeletonHome /> : posts.length ? <HomeWithPost posts={posts} /> : <HomeWithoutPost />}
+      {fetchStatus.isLoading ? (
+        <SkeletonHome />
+      ) : posts.length ? (
+        <>
+          <HomeWithPost posts={posts} />
+          {fetchStatus.isFetching && (
+            <div className={StylesSpinner.spinner}>
+              <div className={StylesSpinner.loading} />
+              <p>Loading</p>
+            </div>
+          )}
+        </>
+      ) : (
+        <HomeWithoutPost />
+      )}
       <Footer />
     </>
   );
