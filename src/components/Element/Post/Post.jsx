2ede73b569137712847f59c18e0e59382db01900
jypark38 <jury124@naver.com>
2024-03-12 17:00:23 +0900

    refactor : 링크 분기 구분 및 컴포넌트 재구성(#98)

--- src/components/Element/Post/Post.jsx
+++ src/components/Element/Post/Post.jsx
@@ -1,5 +1,5 @@
-import React, { useRef } from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import React, { createContext, useRef } from 'react';
+import { Link } from 'react-router-dom';
 
 import { User } from '../User';
 import { ButtonOptionPost } from '../Buttons';
@@ -13,30 +13,24 @@ import { imgReg } from '../../../libs/constant/regex';
 import { storage } from '../../../utils/storage';
 import { useLazyLoad } from '../../../hooks/useLazyLoad';
 
-function withLinkTag(Component, id, text) {
-  return function newComponent(props) {
-    const { pathname } = useLocation();
-
-    if (pathname.includes('post')) return <Component {...props} />;
-
-    return (
-      <Link to={`/post?postId=${id}`}>
-        <span className="a11y-hidden">{text}</span>
-        <Component {...props} />
-      </Link>
-    );
-  };
-}
-
 // 콘텐츠
 
-function returnContentTag() {
-  const { pathname } = useLocation();
+function returnContentTag(path) {
+  switch (path) {
+    case 'profile':
+      return 'h3';
+    case 'post':
+      return 'p';
+    default:
+      return 'h2';
+  }
+}
 
-  if (pathname.includes('home')) return 'h2';
-  if (pathname.includes('profile')) return 'h3';
-  if (pathname.includes('post')) return 'p';
-  return 'h2';
+function TextContent({ path, text }) {
+  // pathname에 따른 콘텐츠 글자 태그 결정
+  const TextTag = returnContentTag(path);
+
+  return <TextTag className="post-text">{text}</TextTag>;
 }
 
 function 이미지전처리(image) {
@@ -46,7 +40,7 @@ function 이미지전처리(image) {
     .map(img => checkImageUrl(img, 'post'));
 }
 
-function Content({ content, image }) {
+function ImageContent({ src }) {
   // lazy load
   const imgRef = useRef([]);
   imgRef.current = [];
@@ -56,15 +50,11 @@ function Content({ content, image }) {
   };
   useLazyLoad(imgRef);
 
-  // pathname에 따른 콘텐츠 글자 태그 결정
-  const ContentTag = returnContentTag();
-
   // 요소 최대 3개
-  const imgs = 이미지전처리(image);
+  const imgs = 이미지전처리(src);
 
   return (
     <>
-      <ContentTag className="post-text">{content}</ContentTag>
       {imgs.map(img => (
         <div key={img} className="img-cover">
           <img className="post-img" data-src={img} alt="" ref={addToRefs} />
@@ -74,36 +64,54 @@ function Content({ content, image }) {
   );
 }
 
+function Contents({ src, text, path }) {
+  return (
+    <>
+      <TextContent text={text} path={path} />
+      <ImageContent src={src} />
+    </>
+  );
+}
 // 댓글 아이콘
-function CommentIcon({ commentCount }) {
+function CommentIcon({ value }) {
   return (
     <>
       <MessageCircle />
-      <span className="cnt">{commentCount}</span>
+      <span className="cnt">{value}</span>
     </>
   );
 }
 
-// Post 링크 분기
-// 게시글 상세 페이지에서는 게시글 상세보기 링크 막기
-export function Post({ post }) {
+function withLinkTag(Component, text) {
+  return function ({ id, ...props }) {
+    return (
+      <Link to={`/post?postId=${id}`}>
+        <span className="a11y-hidden">{text}</span>
+        <Component {...props} />
+      </Link>
+    );
+  };
+}
+
+const ContentsWithLink = withLinkTag(Contents, '게시글 상세보기');
+const CommentIconWithLink = withLinkTag(CommentIcon, '게시물 댓글 보러가기');
+
+const PostContext = createContext();
+console.log(PostContext);
+
+// idx : 게시글 순서 식별용
+export function Post({ post, path = 'home', idx = 0 }) {
   // data
   const { author, commentCount, content, createdAt, heartCount, hearted, id, image } = post;
 
   return (
-    <section id="post" className="home-post" data-postid={id}>
+    <section id={`post${idx}`} className="home-post" data-postid={id}>
       <User category="post" accountName={author.accountname} userName={author.username} detail={author.accountname} profileImg={author.image} />
       <div className="post-edit">
-        <Link to={`/post?postId=${id}`}>
-          <span className="a11y-hidden">게시글 상세보기</span>
-          <Content content={content} image={image} />
-        </Link>
+        <ContentsWithLink src={image} text={content} path={path} />
         <div className="post-icon">
-          <ButtonLike postid={id} hearted={hearted} heartCnts={heartCount} />
-          <Link to={`/post?postId=${id}`}>
-            <span className="a11y-hidden">게시물 댓글 보러가기</span>
-            <CommentIcon commentCount={commentCount} />
-          </Link>
+          <ButtonLike postId={id} hearted={hearted} heartCount={heartCount} />
+          <CommentIconWithLink id={id} value={commentCount} />
         </div>
         {/* 날짜 처리 함수 적용 필요 */}
         <p className="post-date">{dateProcess(createdAt)}</p>
@@ -113,3 +121,15 @@ export function Post({ post }) {
     </section>
   );
 }
+
+// function 게시글({ children, post }) {
+//   const { author, commentCount, content, createdAt, heartCount, hearted, id, image } = post;
+
+//   return (
+//     <section id="post" className="home-post" data-postid={id}>
+//       <User category="post" accountName={author.accountname} userName={author.username} detail={author.accountname} profileImg={author.image} />
+//       <div className="post-edit">{children}</div>
+//       <ButtonOptionPost postid={id} isMyPost={author.accountname === storage.getAccountName()} />
+//     </section>
+//   );
+// }
