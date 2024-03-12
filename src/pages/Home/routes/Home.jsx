693de5386ba8d0bb83ea6678b8cf32f22c2eddd2
ebokyung <dlqhrud303@naver.com>
2024-03-12 20:07:10 +0900

    add: 홈 피드에 withInfiniteScroll 적용 경우 추가

--- src/pages/Home/routes/Home.jsx
+++ src/pages/Home/routes/Home.jsx
@@ -46,3 +46,36 @@ export default function Home() {
     </>
   );
 }
+
+// -------- HoC 사용시 ----------
+
+// /* eslint-disable no-nested-ternary */
+// import React from 'react';
+// import Header from '../../../components/Element/Header/Header';
+// import Footer from '../../../components/Element/Navbar/Navbar';
+// import '../style/Home.scss';
+// import HomeWithPost from '../components/HomeWithPost';
+// import HomeWithoutPost from '../components/HomeWithoutPost';
+// import SkeletonHome from '../components/SkeletonHome';
+// import { Meta } from '../../../libs/Meta';
+// import { SkipNav } from '../../../components/Element/SkipNav/SkipNav';
+// import withInfiniteScroll from '../../../hocs/withInfiniteScroll';
+
+// function HomeFeed({ data }) {
+//   if (!data.length) return <HomeWithoutPost />;
+//   return <HomeWithPost posts={data} />;
+// }
+
+// export default function Home() {
+//   const VIEW_COUNT = 6;
+//   const WithInfiniteHomeFeed = withInfiniteScroll(SkeletonHome, HomeFeed, `/post/feed`, VIEW_COUNT);
+//   return (
+//     <>
+//       <Meta title="피드" />
+//       <SkipNav page="home" />
+//       <Header page="main" text="가지마켓 피드" />
+//       <WithInfiniteHomeFeed />
+//       <Footer />
+//     </>
+//   );
+// }
