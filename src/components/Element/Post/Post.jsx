36ea3daa5ebe0ca7edc175a7535a518ca612dbf5
jypark38 <jury124@naver.com>
2024-03-13 14:21:37 +0900

    (#98) refactor : compound pattern 적용(1)

--- src/components/Element/Post/Post.jsx
+++ src/components/Element/Post/Post.jsx
@@ -1,4 +1,4 @@
-import React, { createContext, useRef } from 'react';
+import React, { createContext, useContext, useRef } from 'react';
 import { Link } from 'react-router-dom';
 
 import './Post.scss';
@@ -7,76 +7,77 @@ import { ButtonOptionPost } from '../Buttons';
 import { ReactComponent as MessageCircle } from '../../../assets/icon/icon-message-circle.svg';
 import { ReactComponent as Heart } from './icon-heart.svg';
 
-import { returnTextContentTag, 이미지전처리 } from './utils';
+import { returnTextTag, 이미지전처리 } from './utils';
 import { dateProcess } from '../../../utils/date';
 import { storage } from '../../../utils/storage';
 import { useLazyLoad } from '../../../hooks/useLazyLoad';
 import useLike from './hooks/useLike';
 
+const PostContext = createContext({
+  id: '',
+  image: '',
+  content: '',
+  hearted: false,
+  heartCount: 0,
+  commentCount: 0,
+  createdAt: '',
+});
+
 // 콘텐츠
 // 텍스트 콘텐츠
-function TextContents({ path, text }) {
-  // pathname에 따른 콘텐츠 글자 태그 결정
-  const TextTag = returnTextContentTag(path);
+function TextContents() {
+  const { path, content } = useContext(PostContext);
 
-  return <TextTag className="post-text">{text}</TextTag>;
+  // pathname에 따른 콘텐츠 글자 태그 결정
+  const TextTag = returnTextTag(path);
+
+  return <TextTag className="post-text">{content}</TextTag>;
 }
 
 // 리팩토링 필요
-function ImageContents({ src }) {
+function ImageContents() {
   // lazy load
   const imgRef = useRef([]);
   imgRef.current = [];
   const addToRefs = el => {
-    console.log(el);
+    // console.log(el);
     imgRef.current.push(el);
   };
   useLazyLoad(imgRef);
 
-  // 요소 최대 3개
-  const imgs = 이미지전처리(src);
+  // 요소 최대 3개 -> 메모이제이션 불필요
+  const { image } = useContext(PostContext);
+  const imgs = 이미지전처리(image);
 
   return (
     <>
       {imgs.map(img => (
-        <Image key={img} src={img} ref={addToRefs} />
+        <div className="img-cover">
+          <img className="post-img" data-src={img} alt="" ref={addToRefs} />
+        </div>
       ))}
     </>
   );
 }
 
-const Image = React.forwardRef(({ src }, ref) => {
-  return (
-    <div className="img-cover">
-      <img className="post-img" data-src={src} alt="" ref={ref} />
-    </div>
-  );
-});
-
-// 콘텐츠 컨테이너
-function Contents({ src, text, path }) {
-  return (
-    <>
-      <TextContents text={text} path={path} />
-      <ImageContents src={src} />
-    </>
-  );
-}
 // 아이콘
 function Count({ value }) {
   return <span className="cnt">{value}</span>;
 }
 
-function Comment({ value }) {
+function Comment() {
+  const { commentCount } = useContext(PostContext);
   return (
     <>
       <MessageCircle />
-      <Count value={value} />
+      <Count value={commentCount} />
     </>
   );
 }
-function Like({ id, status, value }) {
-  const { isHeart, heartCnt, toggleHeart } = useLike({ id, hearted: status, heartCount: value });
+function Like() {
+  const { id, hearted, heartCount } = useContext(PostContext);
+
+  const { isHeart, heartCnt, toggleHeart } = useLike({ id, hearted, heartCount });
 
   return (
     <button type="button" className={`btn-like${isHeart ? ' like' : ''}`} onClick={toggleHeart}>
@@ -88,54 +89,81 @@ function Like({ id, status, value }) {
 
 // 링크 다는 고차컴포넌트
 function withLinkTag(Component, text) {
-  return function ({ id, ...props }) {
+  return function () {
+    const { id } = useContext(PostContext);
     return (
       <Link to={`/post?postId=${id}`}>
         <span className="a11y-hidden">{text}</span>
-        <Component {...props} />
+        <Component />
       </Link>
     );
   };
 }
 
-const ContentsWithLink = withLinkTag(Contents, '게시글 상세보기');
-const CommentWithLink = withLinkTag(Comment, '게시물 댓글 보러가기');
+const ContentsWithLink = withLinkTag(
+  () => (
+    <>
+      <TextContents />
+      <ImageContents />
+    </>
+  ),
+  '게시글 보기',
+);
+const CommentWithLink = withLinkTag(Comment, '댓글 보기');
 
-function Date({ value }) {
-  const date = dateProcess(value);
+function Date() {
+  const { createdAt } = useContext(PostContext);
+  const date = dateProcess(createdAt);
   return <p className="post-date">{date}</p>;
 }
 
-const PostContext = createContext();
-console.log(PostContext);
-
-// idx : 게시글 순서 식별용
-export function Post({ post, path = 'home', idx = 0 }) {
+export function Post({ post, children, path = 'home', idx = 0 }) {
   // data
-  const { author, commentCount, content, createdAt, heartCount, hearted, id, image } = post;
+  const { author, id } = post;
+
+  const contextValue = React.useMemo(() => {
+    return { ...post, path };
+  }, [post, path]);
 
   return (
-    <section id={`post${idx}`} className="home-post" data-postid={id}>
-      <User category="post" accountName={author.accountname} userName={author.username} detail={author.accountname} profileImg={author.image} />
-      <PostContext.Provider>
-        <div className="post-edit">
-          <ContentsWithLink id={id} src={image} text={content} path={path} />
-          {/* icon의 props들은 context 안에 넣게되면 리렌더링이 불필요하게 일어날 일이 생길 수 있음 */}
-          <div className="post-icon">
-            <Like id={id} status={hearted} value={heartCount} />
-            <CommentWithLink id={id} value={commentCount} />
-          </div>
-          {/* 날짜 처리 함수 적용 필요 */}
-          <Date value={createdAt} />
-        </div>
-      </PostContext.Provider>
+    <PostContext.Provider value={contextValue}>
+      <section id={`post${idx}`} className="home-post" data-postid={id}>
+        <User category="post" accountName={author.accountname} userName={author.username} detail={author.accountname} profileImg={author.image} />
+        <div className="post-edit">{children}</div>
+        <ButtonOptionPost postid={id} isMyPost={author.accountname === storage.getAccountName()} />
+      </section>
+    </PostContext.Provider>
+  );
+}
+Post.TextContents = TextContents;
+Post.ImageContents = ImageContents;
+Post.ContentsWithLink = ContentsWithLink;
+Post.Like = Like;
+Post.Comment = Comment;
+Post.CommentWithLink = CommentWithLink;
+Post.Date = Date;
 
-      <ButtonOptionPost postid={id} isMyPost={author.accountname === storage.getAccountName()} />
-    </section>
+export function PostWithoutLink({ post }) {
+  return (
+    <Post post={post} path="post">
+      <Post.TextContents />
+      <Post.ImageContents />
+      <div className="post-icon">
+        <Post.Like />
+        <Post.Comment />
+      </div>
+    </Post>
   );
 }
 
-/**
- * PostWithLink
- * PostWithoutLink
- */
+export function PostWithLink({ post, path, idx }) {
+  return (
+    <Post post={post} path={path} idx={idx}>
+      <Post.ContentsWithLink />
+      <div className="post-icon">
+        <Post.Like />
+        <Post.CommentWithLink />
+      </div>
+    </Post>
+  );
+}
