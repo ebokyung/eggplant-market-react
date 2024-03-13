f00214d738c066ba4f30fc7f576cf7f29f4dbdd3
ebokyung <dlqhrud303@naver.com>
2024-03-13 16:22:34 +0900

    refactor: 상태 범위를 줄이기 위해 팔로우 페이지에서 팔로우 리스트 컴포넌트 분리

--- src/pages/Follow/components/FollowList.jsx
+++ src/pages/Follow/components/FollowList.jsx
@@ -1,10 +1,38 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import { useSearchParams } from 'react-router-dom';
+import { defaultAxios } from '../../../libs/api/axios';
+import { useScroll } from '../../../hooks/useScroll';
 import FollowItem from './FollowItem';
+import SkeletonFollowItem from './SkeletonFollowItem';
+import Spinner from '../../../components/Element/Spinner/Spinner';
+import '../style/Follow.scss';
 
-export default function FollowList({ data }) {
-  return data.map(item => (
-    <li key={item._id} className="follow-item">
-      <FollowItem user={item} />
-    </li>
-  ));
+const VIEW_COUNT = 11;
+
+export default function FollowList({ isFollower }) {
+  const [searchParams] = useSearchParams();
+  const accountName = searchParams.get('accountName');
+
+  const fetchFollow = useCallback(async page => {
+    return defaultAxios.get(`/profile/${accountName}/${isFollower ? 'follower' : 'following'}?skip=${page * VIEW_COUNT}&limit=${VIEW_COUNT}`);
+  });
+
+  const { data, fetchStatus } = useScroll(fetchFollow);
+
+  return (
+    <ul className="follow-list">
+      {fetchStatus.isLoading ? (
+        <SkeletonFollowItem />
+      ) : (
+        <>
+          {data.map(item => (
+            <li key={item._id} className="follow-item">
+              <FollowItem user={item} />
+            </li>
+          ))}
+          {fetchStatus.isFetching && <Spinner />}
+        </>
+      )}
+    </ul>
+  );
 }
--- src/pages/Follow/routes/Follow.jsx
+++ src/pages/Follow/routes/Follow.jsx
@@ -4,26 +4,18 @@ import Header from '../../../components/Element/Header/Header';
 import Navbar from '../../../components/Element/Navbar/Navbar';
 import FollowList from '../components/FollowList';
 import '../style/Follow.scss';
-import SkeletonFollowItem from '../components/SkeletonFollowItem';
 import { Meta } from '../../../libs/Meta';
-import withInfiniteScroll from '../../../hocs/withInfiniteScroll';
 
 export default function Follow() {
   const location = useLocation();
   const isFollower = location.pathname.includes('follower');
-  const accountName = new URLSearchParams(location.search).get('accountName');
-
-  const VIEW_COUNT = 11;
-  const WithInfinityScrollFollowList = withInfiniteScroll(SkeletonFollowItem, FollowList, `/profile/${accountName}/${isFollower ? 'follower' : 'following'}`, VIEW_COUNT);
 
   return (
     <>
       <Meta title={`${isFollower ? '팔로워' : '팔로잉'} 목록`} />
       <Header page="follow" text={isFollower ? 'Follower' : 'Following'} />
       <main className="main-with-nav">
-        <ul className="follow-list">
-          <WithInfinityScrollFollowList />
-        </ul>
+        <FollowList isFollower={isFollower} />
       </main>
       <Navbar />
     </>
