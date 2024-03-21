96dff7bb0a1646302bcb1aa71610e17b222f9a28
jypark38 <jury124@naver.com>
2024-03-21 16:23:46 +0900

    docs : 주석 추가

--- src/components/Element/User/User.jsx
+++ src/components/Element/User/User.jsx
@@ -8,6 +8,7 @@ import './style/User.scss';
 // 링크 분기 : profile 페이지의 경우 링크 작동 x
 // follow의 경우 UserInfo 내부의 p태크는 링크 작동 x
 export function User({ category, accountName, userName, detail, profileImg }) {
+  // comment-user-info ??
   const linkClassName = category === 'comment' ? 'comment-user-info' : 'user-container';
 
   return (
