aa518b454600160906a3b9fac06c4f46f08973cb
jypark38 <jury124@naver.com>
2024-03-26 18:36:43 +0900

    refactor : search시 최초로 등장하는 Keyword만 strong으로 감싸도록 변경(#100)

--- src/pages/Search/components/SearchItem.jsx
+++ src/pages/Search/components/SearchItem.jsx
@@ -2,33 +2,39 @@ import React from 'react';
 // import { User } from '../../../components/Element/User';
 import { SearchUser } from '../../../components/Element/User';
 
-// 문제 있어보이는디?
-/**
- * ! split을 하면 안될거 같음
- */
+function highlightKeyword(inputText, searchText) {
+  const index = inputText.indexOf(searchText);
 
-function highlightKeyword(text, keyword) {
-  if (text.indexOf(keyword) !== -1) {
-    const parts = text.split(keyword);
-    return (
-      <>
-        {parts[0]}
-        <strong>{keyword}</strong>
-        {parts[1]}
-      </>
-    );
+  // searchText를 찾지 못한 경우, 원본 문자열을 반환
+  if (index === -1) {
+    return inputText;
   }
-  return text;
+
+  // 시작 부분부터 검색어 시작 인덱스까지의 문자열
+  const start = inputText.substring(0, index);
+
+  // 검색어 끝나는 부분부터 문자열 끝까지
+  const end = inputText.substring(index + searchText.length);
+
+  // 강조할 텍스트를 <strong> 태그로 감싸서 전체 문자열을 재조합
+  return (
+    <>
+      {start}
+      <strong>{searchText}</strong>
+      {end}
+    </>
+  );
 }
 
 export default function SearchItem({ user, keyword }) {
-  const { _id, username } = user;
-  const content = highlightKeyword(username, keyword);
-  console.log(content);
+  const { _id } = user;
+
+  const author = { ...user };
+  author.username = highlightKeyword(author.username, keyword);
 
   return (
     <li key={_id}>
-      <SearchUser author={user} />
+      <SearchUser author={author} />
     </li>
   );
 }
